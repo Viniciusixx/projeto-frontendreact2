b5206964411a6909dbaf6b114736ea8bc60dd034
Viniciusixx <vinitotheworld@gmail.com>
2022-11-26 17:05:30 -0300

    finish

---
+++ react-app-vini/src/componetes/Main/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Home from "../Home"
+
+import { Main2, Mae3, Img, ImgMais, TextCortados, ImgLinImg } from "./slyled";
+
+
+function Main() {
+    return(
+        <>
+        <Home/>
+        </>
+    )
+}
+
+export default Main
